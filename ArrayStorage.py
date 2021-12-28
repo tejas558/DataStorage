5d23a9d9d53d3b263b0e0f53545ebd9f6553c946
ttorpy <88541385+ttorpy@users.noreply.github.com>
2021-12-28 13:03:31 +0900

    print updates

--- ArrayStorage.py
+++ ArrayStorage.py
@@ -5,6 +5,7 @@ while True:
     print("type f to find")
     print("type d to delete")
     print("type p to print all")
+    print("type anything else to exit")
     print()
     x = input('Input your operation: ')
     if x == 'i':
@@ -15,13 +16,13 @@ while True:
             storage.append(val)
             print("this value has been added to storage")
     elif x == 'f':
-        val = input('type the value taht you wish to find')
+        val = input('type the value taht you wish to find: ')
         if val in storage:
             print('this value has been found')
         else:
             print("this value was not found in storage")
     elif x == 'd':
-        val = input("type the value that you would like to delete")
+        val = input("type the value that you would like to delete: ")
         if val not in storage:
             print("failed to delete", val, "this value was not in storage")
         else:
@@ -30,3 +31,4 @@ while True:
         print(storage)
     else:
         break
+    print()
